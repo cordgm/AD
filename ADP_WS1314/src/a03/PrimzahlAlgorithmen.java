19362e610d37c23d688605480a8aff77b94ea614
cordgm <cordgm@gmail.com>
2013-10-12 15:22:46 +0200

    leere Methoden

---
+++ ADP_WS1314/src/a03/PrimzahlAlgorithmen.java
@@ -0,0 +1,50 @@
+package a03;
+
+public class PrimzahlAlgorithmen {
+
+	//Attribute
+	private boolean[] zahlen;
+	private int zaehler;	//fuer die Aufwandsbestimmung
+	
+	//Kontruktor
+	public PrimzahlAlgorithmen() {
+		super();
+		zaehler = 0;
+	}
+	
+	//Methoden
+	/**
+	 * langsames Primzahl suchen
+	 * @param n Problemgroesse
+	 */
+	public void searchPrimSlow(int n){
+		
+	}
+
+	/**
+	 * schnelles Primzahl suchen
+	 * @param n Problemgroesse
+	 */
+	public void searchPrimFast(int n){
+		
+	}
+	
+	/**
+	 * Sieb des Eratosthenes
+	 * @param n Problemgroesse
+	 */
+	public void searchPrimSiebDesEratosthenes(int n){
+		
+	}
+	
+	/**
+	 * Primzahleigenschaft feststellen
+	 * @param n Problemgroesse
+	 * @return
+	 */
+	public boolean primality(int n){
+		//todo
+		return false;
+		
+	}
+}
