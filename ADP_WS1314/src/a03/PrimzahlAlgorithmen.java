0623c101816d9834e4a685d22df209755d363e12
cordgm <cordgm@gmail.com>
2013-10-12 16:32:33 +0200

    ersten 3 Algorithmen wirklich fertig

--- ADP_WS1314/src/a03/PrimzahlAlgorithmen.java
+++ ADP_WS1314/src/a03/PrimzahlAlgorithmen.java
@@ -42,7 +42,7 @@ public class PrimzahlAlgorithmen {
 		for(int i = 0; i < n ; i++)
 			zahlen[i] = true;
 		for(int i = 2; i < n; i++){
-			for(int j = 2; j < i; j++){
+			for(int j = 2; j < Math.sqrt(i); j++){
 				if((i%j == 0) && (j!=i)){
 					zahlen[i] = false;
 					zaehler++;
--- ADP_WS1314/src/a03/Testframe.java
+++ ADP_WS1314/src/a03/Testframe.java
@@ -15,10 +15,40 @@ public class Testframe {
 		System.out.println("1000        : "+primzahlAlgorithmen.getZaehler());
 		primzahlAlgorithmen.searchPrimSlow(10000);
 		System.out.println("10000       : "+primzahlAlgorithmen.getZaehler());
-		primzahlAlgorithmen.searchPrimSlow(100000);
-		System.out.println("100000      : "+primzahlAlgorithmen.getZaehler());
-		primzahlAlgorithmen.searchPrimSlow(1000000);
-		System.out.println("1000000     : "+primzahlAlgorithmen.getZaehler());
+//		primzahlAlgorithmen.searchPrimSlow(100000);
+//		System.out.println("100000      : "+primzahlAlgorithmen.getZaehler());
+//		primzahlAlgorithmen.searchPrimSlow(1000000);
+//		System.out.println("1000000     : "+primzahlAlgorithmen.getZaehler());
+		
+		
+		System.out.println("\n****** Testframe ******\nschnelles Primzahl suchen:");
+		primzahlAlgorithmen.searchPrimFast(10);
+		System.out.println("10          : "+primzahlAlgorithmen.getZaehler());
+		primzahlAlgorithmen.searchPrimFast(100);
+		System.out.println("100         : "+primzahlAlgorithmen.getZaehler());
+		primzahlAlgorithmen.searchPrimFast(1000);
+		System.out.println("1000        : "+primzahlAlgorithmen.getZaehler());
+		primzahlAlgorithmen.searchPrimFast(10000);
+		System.out.println("10000       : "+primzahlAlgorithmen.getZaehler());
+//		primzahlAlgorithmen.searchPrimFast(100000);
+//		System.out.println("100000      : "+primzahlAlgorithmen.getZaehler());
+//		primzahlAlgorithmen.searchPrimFast(1000000);
+//		System.out.println("1000000     : "+primzahlAlgorithmen.getZaehler());
+		
+		
+		System.out.println("\n****** Testframe ******\nSieb des Eratosthenes");
+		primzahlAlgorithmen.searchPrimSiebDesEratosthenes(10);
+		System.out.println("10          : "+primzahlAlgorithmen.getZaehler());
+		primzahlAlgorithmen.searchPrimSiebDesEratosthenes(100);
+		System.out.println("100         : "+primzahlAlgorithmen.getZaehler());
+		primzahlAlgorithmen.searchPrimSiebDesEratosthenes(1000);
+		System.out.println("1000        : "+primzahlAlgorithmen.getZaehler());
+		primzahlAlgorithmen.searchPrimSiebDesEratosthenes(10000);
+		System.out.println("10000       : "+primzahlAlgorithmen.getZaehler());
+//		primzahlAlgorithmen.searchPrimSlow(100000);
+//		System.out.println("100000      : "+primzahlAlgorithmen.getZaehler());
+//		primzahlAlgorithmen.searchPrimSlow(1000000);
+//		System.out.println("1000000     : "+primzahlAlgorithmen.getZaehler());
 	}
 
 }
