7ddbb9b90ded49e9cd686a42c4c977051a254bf0
cordgm <cordgm@gmail.com>
2013-10-27 14:24:01 +0100

    hab bisher nur ne Unterfunktion von 3.

--- ADP_WS1314/src/a05/PascalBinomal.java
+++ ADP_WS1314/src/a05/PascalBinomal.java
@@ -1,5 +1,28 @@
 package a05;
 
 public class PascalBinomal {
+	
+	/**
+	 * n ueber k-1
+	 * P(n,r) = n! / (n - r)!
+	 * @param n
+	 */
+	public void pascalscheDreieckSchneller(int n){
+		if(n>=0){
+			
+		}
+	}
+	
+	private int fakultaet(int n){
+		int fakultaet = 1;
+		if(n >= 0){
+			while(n > 1){
+		        fakultaet = fakultaet * n;
+		        n = n - 1;
+			}
+			return fakultaet;
+		}
+		return -1;
+	}
 
 }
