7f10b6070aa8e58acce4448b5a0f1eeb7fdca752
cordgm <cordgm@gmail.com>
2013-11-02 14:40:48 +0100

    und DAS Element mit Schluessel

---
+++ ADP_WS1314/src/a06/ListElement.java
@@ -0,0 +1,22 @@
+package a06;
+
+/**
+ * 
+ * @author Manuel Meyer und Cord Godehus-Meyer
+ *
+ */
+public class ListElement {
+	
+	//Attribute
+	private int schluessel;
+
+	//Konstruktor
+	public ListElement(int schluessel) {
+		this.schluessel = schluessel;
+	}
+
+	@Override
+	public String toString() {
+		return schluessel + ", ";
+	}
+}
