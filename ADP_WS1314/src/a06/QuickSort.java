6a47986cc05ff1075e8d8faff7439148fdf60b05
cordgm <cordgm@gmail.com>
2013-11-02 14:34:07 +0100

    leere Klasse und Methoden

---
+++ ADP_WS1314/src/a06/QuickSort.java
@@ -0,0 +1,33 @@
+package a06;
+
+/**
+ * 
+ * @author Manuel Meyer und Cord Godehus-Meyer
+ * @version 0
+ * @param <T>
+ */
+public class QuickSort<T> {
+	
+	//Attribute
+	private T[] array;
+	private int aufwandZaehler;
+
+	//Konstruktor
+	public QuickSort(T[] array) {
+		this.array = array;
+		this.aufwandZaehler = 0;
+	}
+	
+	public void QuickSort1(){
+		
+	}
+	
+	public void QuickSort2(){
+		
+	}
+	
+	public void QuickSort3(){
+		
+	}
+
+}
---
+++ ADP_WS1314/src/a06/test/JUnit_QuickSort.java
@@ -0,0 +1,24 @@
+package a06.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ * @author Manuel Meyer und Cord Godehus-Meyer
+ * @version 0
+ */
+public class JUnit_QuickSort {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
