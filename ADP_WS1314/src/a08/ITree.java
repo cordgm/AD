db42b2b45248fab0e425d932440efec87411a6d6
Warmaker <Manuel.Meyer@haw-hamburg.de>
2013-11-17 14:55:01 +0100

    no message

---
+++ ADP_WS1314/src/a08/ITree.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package a08;
+
+/**
+ * @author Colonel_Sheppard
+ * Das Interface soll folgenedes ermöglichen. 
+ * Knoten hinzufügen. 
+ * Ausgabe von Haupt- Nebenreihenfolge und symmetrisch. 
+ * Ausgabe der Söhne und dessen Vater. 
+ * @param <T>
+ *
+ */
+public interface ITree<T> {
+    
+    /**
+     * Ausgabe der Knoten in Symmetrischer Folge. 
+     * @param vater
+     */
+    void inorder( Node<T> vater);
+    
+    /**
+     * Ausgabe der Knoten in der Hauptreihenfolge. 
+     * @param vater
+     */
+    void preorder( Node<T> vater);
+    
+    /**
+     * Ausgabe der Knoten in der Hauptreihenfolge. 
+     * @param vater
+     */
+    void postorder( Node<T> vater);
+    
+    /**
+     * @param n
+     * @return
+     */
+    public int getLinkerSohn(int n);
+
+    /**
+     * @param n
+     * @return
+     */
+    public int getRechterSohn(int n);
+
+    /**
+     * @param n
+     * @return
+     */
+    public Node<T> getVater(int n);
+
+}
