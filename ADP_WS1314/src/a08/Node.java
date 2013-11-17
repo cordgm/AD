bdec7a27c0d2dfb33d8a87c0b5f84591c17887f5
Warmaker <Manuel.Meyer@haw-hamburg.de>
2013-11-17 14:21:48 +0100

    no message

--- ADP_WS1314/src/a08/Node.java
+++ ADP_WS1314/src/a08/Node.java
@@ -1,5 +1,10 @@
 package a08;
 
-public class Node {
+public class Node<T> {
 
+    T daten; //Die eigentliche Daten
+    int key; 
+    Node<T> links;    //Linker Sohn
+    Node<T> rechts;  //Rechter Sohn
+    Node<T> vater;   //Vater (Knoten)
 }
