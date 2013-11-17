74283bed54373062fde101ce353d8cfbaffef44f
cordgm <cordgm@gmail.com>
2013-11-17 15:17:58 +0100

    no message

--- ADP_WS1314/src/a08/test/JUnit_Tree.java
+++ ADP_WS1314/src/a08/test/JUnit_Tree.java
@@ -14,15 +14,15 @@ import a08.*;
  * @author cordgm
  *
  */
-public class JUnit_Binaerer_Suchbaum {
+public class JUnit_Tree {
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
-		IBinaerer_Suchbaum<Element> bsBaumA = new Binaerer_Suchbaum_Array<Element>();
-		IBinaerer_Suchbaum<Element> bsBaumV = new Binaerer_Suchbaum_Verlinkung<Element>();
+		ITree<Element> bsBaumA = new TreeArray<Element>();
+//		ITree<Element> bsBaumV = new Binaerer_Suchbaum_Verlinkung<Element>();
 		for(int i = 0; i < 3; i++){
 			Node<Element> node = new Node<Element>(new Element(i), i, null, null, null);
 			
