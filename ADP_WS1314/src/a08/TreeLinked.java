ced02ddc928bae08e31d60f33fbeec26b633a722
Warmaker <Manuel.Meyer@haw-hamburg.de>
2013-11-17 15:20:30 +0100

    no message

---
+++ ADP_WS1314/src/a08/TreeLinked.java
@@ -0,0 +1,47 @@
+package a08;
+
+public class TreeLinked<T> implements ITree<T> {
+
+    @Override
+    public void insert(Node<T> key) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void inorder(Node<T> vater) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void preorder(Node<T> vater) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void postorder(Node<T> vater) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int getLinkerSohn(int n) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getRechterSohn(int n) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Node<T> getVater(int n) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
