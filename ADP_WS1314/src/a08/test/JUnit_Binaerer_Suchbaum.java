8c56e33dfedb4837a53b04bb4725067336ea8b89
cordgm <cordgm@gmail.com>
2013-11-17 14:37:38 +0100

    Element (fertig) und Test (nicht fertig)

---
+++ ADP_WS1314/src/a08/test/JUnit_Binaerer_Suchbaum.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package a08.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author cordgm
+ *
+ */
+public class JUnit_Binaerer_Suchbaum {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
