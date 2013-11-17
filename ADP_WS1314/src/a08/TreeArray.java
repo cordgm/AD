358ab27fd1e1b118cd3c3d09d72ab8cd62dd822b
Warmaker <Manuel.Meyer@haw-hamburg.de>
2013-11-17 18:53:45 +0100

    TreeArray Insert fertig!

--- ADP_WS1314/src/a08/TreeArray.java
+++ ADP_WS1314/src/a08/TreeArray.java
@@ -3,27 +3,81 @@ package a08;
 public class TreeArray<T> implements ITree<T> {
 
     private Node[] binaryTree;
-    private int N; //Maximale Größe des Arrays
+    private static int N; //Maximale Größe des Arrays
+    private static final int DEFAULT_ARRAY_GROESSE = N * 2;
     
+
     /**
      * 
+     */
+    public TreeArray() {
+        this(DEFAULT_ARRAY_GROESSE);
+    }
+    
+    /**
+     * @param N
      */
     public TreeArray(int N) {
         super();
         this.binaryTree = (Node[]) new Object[N];
     }
+    
 
     @Override
     public boolean insert(Node<T> node ) {
-        int n = 0; 
+        int n = 1; 
         Node<T> tmpNode = node; 
         
         if( node.getKey() == binaryTree[n].getKey() ) return true;
+        
         else {
             
+            while( binaryTree[n] != null ) {
+                
+                if( tmpNode.getKey() < binaryTree[n].getKey() ) { // '<' LINKS
+                    
+                    n = ( n*2 > N ) ? increase(n) : (n*2); //Sohn zur linken Seite
+                    
+                } else { // '>' RECHTS
+                    
+                    n = ( n*2+1 > N ) ? increase(n) : (n*2+1); //Sohn zur Rechten Seite
+                    
+                } 
+            }//End-While
+
+            //INSERT 
+            binaryTree[n] = node; //Neuen Knoten hinzufügen
+            
+            if (n % 2 != 0 && n / 2 > 0) {
+                binaryTree[n/2].setRechts(node); // [n/2]
+            } else if (n % 2 == 0 && n / 2 > 0) {
+                binaryTree[n/2].setLinks(node); // [n/2]
+            }
         }
         
         return true;
+    }
+
+    /**
+     * @param n2
+     * @return
+     */
+    private int increase(int n2) {
+     
+        
+        Node[] hilfsBinaryTree = (Node[]) new Object[N + 
+                                                          DEFAULT_ARRAY_GROESSE];
+        
+        /* Zeiger auf binaryTreeeinträge kopieren */
+        System.arraycopy(binaryTree, 0, hilfsBinaryTree, 0, binaryTree.length);
+        
+        /* Objektvariable binaryTree das neue Array zuweisen */
+        binaryTree = hilfsBinaryTree;
+        System.out.println("binaryTree vergößert auf " +
+                                                       binaryTree.length);
+        
+       return (n2%2==0) ? (n2*2) : ((n2*2)+1);
+        
     }
 
     @Override
