192bff004842b7d813c9b96d54cfad81a073043a
Warmaker <Manuel.Meyer@haw-hamburg.de>
2013-12-15 13:36:45 +0100

    no message

--- ADP_WS1314/src/a12/manuUcord/IBlock_Chiffre.java
+++ ADP_WS1314/src/a12/manuUcord/IBlock_Chiffre.java
@@ -7,6 +7,13 @@ package a12.manuUcord;
  * @author Colonel_Sheppard
  *
  */
-public interface IBlock_Chiffre {
+public interface IBlock_Chiffre<T> {
 
+
+    /**
+     * Umwandlung Text in Int-Array. 
+     * @return int[].
+     */
+    public int[] convertText(String text);
+    
 }
